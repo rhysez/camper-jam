57514be1ce4dd447b2c2b1ddc5bd810f9e46e343
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2026-03-10 11:53:26 +0000

    implement FriendshipFactory

--- tests/Feature/FriendshipTest.php
+++ tests/Feature/FriendshipTest.php
@@ -5,14 +5,13 @@ use App\Models\User;
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-it('allows a user to send a friend request', function () {
+test('allows a user to send a friend request', function () {
     $sender = User::factory()->create();
     $receiver = User::factory()->create();
 
-    $friendship = Friendship::create([
+    $friendship = Friendship::factory()->create([
         'user_id' => $sender->id,
         'friend_id' => $receiver->id,
-        'status' => 'pending',
     ]);
 
     expect($friendship->isPending())->toBeTrue();
